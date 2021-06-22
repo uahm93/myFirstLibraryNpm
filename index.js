61a8e89ee5861f53506d99814ff8f3e939ff01df
Ulises Hernandez <uahm93@gmail.com>
2021-06-22 17:57:51 -0500

    Modulo sencillo con saludos creado

---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = {
+    hola: function(){
+        console.log("hola mundo!!!!");
+    },
+
+    holaPersonalizado: function(nombre){
+        console.log(`Hola ${nombre}`);
+    },
+
+    holaAlCursoNPM: function (){
+        console.log("Hola al curso npm");
+    }
+}
